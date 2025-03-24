5ca0e61a4bc1a6628e4bebb42df216afdf341710
Kush <153984588+kush-singhy@users.noreply.github.com>
2025-03-24 12:15:48 +1100

    Egg timer working

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    let boilTimes = ["Soft": 3, "Medium": 5, "Hard": 7];
+    var timeElapsed = 0
+    var totalTime = 0
+    var timer = Timer()
     
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        let selectedHardness = sender.currentTitle!
+        totalTime = boilTimes[selectedHardness]!
+        
+        progressBar.progress = 0.0
+        timeElapsed = 0
+        titleLabel.text = "Boiling \(selectedHardness) Egg"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+        
+        
 
+        
+    }
+    
+    @objc func updateCounter() {
+        if (timeElapsed < totalTime) {
+            timeElapsed += 1
+            let percentageProgress = Float(timeElapsed) / Float(totalTime)
+            progressBar.progress = percentageProgress
+        } else {
+            titleLabel.text = "DONE"
+        }
+
+    }
+    
 
 }
